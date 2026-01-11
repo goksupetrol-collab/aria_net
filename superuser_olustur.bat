1caca586ae3f6ecc1a832cb8cb38c1880c7b8f87
Telerik Proje <telerik@proje.local>
2026-01-11 16:20:52 +0300

    Tanker ekleme script'leri duzeltildi - py komutu ve venv kullanimi

---
+++ superuser_olustur.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d D:\tayfun
+echo Django Superuser Olusturuluyor...
+echo.
+python manage.py createsuperuser
+pause
---
+++ tanker_ekle.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d D:\tayfun
+echo Tanker MenuItem ekleniyor...
+echo.
+py tanker_ekle_direct.py
+echo.
+pause
